fe5e86af942ab4f78683f51b82f30250a13aa009
Venkatesh Nannan <nvenky@gmail.com>
2017-02-19 19:25:02 +1100

    Add contact form

--- app/components/contact/contact.component.ts
+++ app/components/contact/contact.component.ts
@@ -1,8 +1,11 @@
 import { Component } from '@angular/core';
+import { Contact } from './../../models/contact';
 
 @Component({
     moduleId: module.id,
     selector: 'contact',
     templateUrl: 'contact.component.html'
 })
-export class ContactComponent  {}
+export class ContactComponent  {
+  public model = new Contact('', '', '');
+}
